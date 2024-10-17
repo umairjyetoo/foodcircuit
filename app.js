5ae4d6c147c6dd55a11fac3cee666343f85ea3ba
Umair Jan Yetoo <umair@1-platform.com>
2024-10-17 16:26:36 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve the PDF when the root URL is accessed
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'files', 'food_circuit.pdf'); // PDF location
+  res.sendFile(filePath);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
