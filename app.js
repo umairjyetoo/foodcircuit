2116308878041923999c9c700570ce55d70eef82
Umair Jan Yetoo <umair@1-platform.com>
2024-10-17 17:05:19 +0400

    mionr fix

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve the PDF when the root URL is accessed
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'files', 'foodcircuit.pdf'); // PDF location
-  res.sendFile(filePath);
+    const filePath = path.join(__dirname, 'public', 'foodcircuit.pdf'); // PDF location
+    res.sendFile(filePath);
 });
+
+// Serve the PDF when the root URL is accessed
+// app.get('/', (req, res) => {
+//   const filePath = path.join(__dirname, 'files', 'foodcircuit.pdf'); // PDF location
+//   res.sendFile(filePath);
+// });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
